a4b4af5866a90802949cafd09cd646f04af41d90
jack <jackjack5810@yahoo.com>
2021-05-07 09:34:46 -0400

    intitial commit

--- src/shaders.ts
+++ src/shaders.ts
@@ -20,6 +20,7 @@ export const Shaders = () => {
 
     const fragment = `
         [[location(0)]] var<out> outColor : vec4<f32>;
+        
         [[stage(fragment)]]
         fn main() -> void {
             outColor = vec4<f32>(1.0, 1.0, 0.0, 1.0);
