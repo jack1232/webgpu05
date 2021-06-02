106426864acc1a3549b9ecf2571a2d50fd4189cc
jack <jackjack5810@yahoo.com>
2021-06-02 13:25:22 -0400

    code update

--- src/shaders.ts
+++ src/shaders.ts
@@ -1,5 +1,30 @@
 export const Shaders = () => {
     const vertex = `
+    let pos : array<vec2<f32>, 6> = array<vec2<f32>, 6>(             
+        vec2<f32>(-0.5,  0.7),
+        vec2<f32>( 0.3,  0.6),
+        vec2<f32>( 0.5,  0.3),
+        vec2<f32>( 0.4, -0.5),
+        vec2<f32>(-0.4, -0.4),
+        vec2<f32>(-0.3,  0.2)
+    );
+
+    [[stage(vertex)]]
+    fn main([[builtin(vertex_index)]] VertexIndex: u32) -> [[builtin(position)]] vec4<f32> {
+      return vec4<f32>(pos[VertexIndex], 0.0, 1.0);
+    }`
+
+    const fragment = `
+        [[stage(fragment)]]
+        fn main() ->  [[location(0)]] vec4<f32> {
+            return vec4<f32>(1.0, 1.0, 0.0, 1.0);
+        }
+    `;
+    return {vertex, fragment};
+}
+
+export const ShadersOld = () => {
+    const vertex = `
     const pos : array<vec2<f32>, 6> = array<vec2<f32>, 6>(             
         vec2<f32>(-0.5,  0.7),
         vec2<f32>( 0.3,  0.6),
