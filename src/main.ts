f46adc0335829ba2c482639a351040b31069fd17
jack <jackjack5810@yahoo.com>
2021-08-20 12:24:00 -0400

    update

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ const CreatePrimitive = async (primitiveType = 'point-list') => {
     const canvas = document.getElementById('canvas-webgpu') as HTMLCanvasElement;
     const adapter = await navigator.gpu?.requestAdapter() as GPUAdapter;       
     const device = await adapter?.requestDevice() as GPUDevice;
-    const context = canvas.getContext('gpupresent') as GPUPresentationContext;
+    const context = canvas.getContext('gpupresent') as GPUCanvasContext;
 
     const format = 'bgra8unorm';
     context.configure({
