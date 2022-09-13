4e22d766b5801007a11c1ace127bdd1fdff8d9bf
jack1232 <jackjack5810@yahoo.com>
2022-09-13 15:11:42 -0400

    update 0.1.21

--- src/main.ts
+++ src/main.ts
@@ -23,10 +23,12 @@ const CreatePrimitive = async (primitiveType = 'point-list') => {
     context.configure({
         device: device,
         format: format,
+        alphaMode:'opaque'
     });
 
     const shader = Shaders();
     const pipeline = device.createRenderPipeline({
+        layout:'auto',
         vertex: {
             module: device.createShaderModule({
                 code: shader.vertex
@@ -54,13 +56,14 @@ const CreatePrimitive = async (primitiveType = 'point-list') => {
     const renderPass = commandEncoder.beginRenderPass({
         colorAttachments: [{
             view: textureView as GPUTextureView,
-            loadValue: [0.5, 0.5, 0.8, 1], //background color
+            clearValue: [0.5, 0.5, 0.8, 1], //background color
+            loadOp:'clear',
             storeOp: 'store'
         }]
     });
     renderPass.setPipeline(pipeline);
     renderPass.draw(6);
-    renderPass.endPass();
+    renderPass.end();
     
     device.queue.submit([commandEncoder.finish()]);
 }
--- src/shaders.ts
+++ src/shaders.ts
@@ -1,7 +1,7 @@
 export const Shaders = () => {
     const vertex = `
-        [[stage(vertex)]]
-        fn main([[builtin(vertex_index)]] VertexIndex: u32) -> [[builtin(position)]] vec4<f32> {
+        @vertex
+        fn main(@builtin(vertex_index) VertexIndex: u32) -> @builtin(position) vec4<f32> {
             var pos = array<vec2<f32>, 6>(             
                 vec2<f32>(-0.5,  0.7),
                 vec2<f32>( 0.3,  0.6),
@@ -14,8 +14,8 @@ export const Shaders = () => {
         }`;
 
     const fragment = `
-        [[stage(fragment)]]
-        fn main() ->  [[location(0)]] vec4<f32> {
+        @fragment
+        fn main() ->  @location(0) vec4<f32> {
             return vec4<f32>(1.0, 1.0, 0.0, 1.0);
         }`;
     return {vertex, fragment};
